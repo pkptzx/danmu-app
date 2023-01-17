e494d2992c0ee5c0246937cd7eaf0d88d5a2f3a9
码魂 <pkptzx@gmail.com>
2023-01-18 01:09:05 +0800

    使窗口topbar的按钮支持配置

--- src/components/Window.vue
+++ src/components/Window.vue
@@ -2,19 +2,19 @@
   <div class="win7">
   <div class="window glass active" style="box-shadow: unset;">
     <div data-tauri-drag-region class="title-bar">
-      <div data-tauri-drag-region class="title-bar-text"><img data-tauri-drag-region :src="icon!=null?icon:'/src/assets/vue.svg'" style="width: 16px;height: 16px;border: 0px" />{{
+      <div data-tauri-drag-region class="title-bar-text"><img data-tauri-drag-region :src="icon!=null?icon:'../src/assets/vue.svg'" style="width: 16px;height: 16px;border: 0px" />{{
         title
       }}</div>
       <div class="title-bar-controls">
-        <button aria-label="Top" @click="onTop">
+        <button v-if="!hideTop" aria-label="Top" @click="onTop">
           <q-icon name="o_push_pin" color="white" :class="{'rotate-315':wintop}" size="1.2em" style="text-shadow: 0px 0px 2px black,1px 0px gray,0px 1px gray,-1px 0px gray,0px -1px gray;"></q-icon>
         </button>
-        <button aria-label="Minimize" @click="onMinimize"></button>
-        <button aria-label="Maximize" @click="onMaximize"></button>
+        <button v-if="!hideMinimize" aria-label="Minimize" @click="onMinimize"></button>
+        <button v-if="!hideMaximize" aria-label="Maximize" @click="onMaximize"></button>
         <button aria-label="Close" @click="onClose"></button>
       </div>
     </div>
-    <div :class="['window-body',{'has-space':space},bgColor]" style="height: calc(100vh - 40px);">
+    <div :class="['window-body',{'has-space':!noSpace},bgColor]" style="height: calc(100vh - 40px);">
       <slot>Window 窗体内容</slot>
     </div>
   </div>
@@ -26,13 +26,17 @@ import { ref, watch } from "vue";
 import { confirm } from '@tauri-apps/api/dialog';
 import { exit } from '@tauri-apps/api/process';
 import { appWindow } from '@tauri-apps/api/window';
-defineProps({
-  title: String,
-  icon: String,
-  bgColor: String,
-  "space": { type: Boolean, required: false, default: true },
-})
+const props = defineProps<{
+  title: string,
+  icon?: string,
+  bgColor?: string,
+  noSpace?: boolean,
+  hideTop?: boolean,
+  hideMinimize?: boolean,
+  hideMaximize?: boolean,
+}>()
 const wintop = ref(false);
+appWindow.setResizable(!props.hideMaximize);
 appWindow.setAlwaysOnTop(wintop.value);
 watch(wintop, (newTop) => {
     appWindow.setAlwaysOnTop(newTop);
