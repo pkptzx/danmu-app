08cc87de3a89498ad97d72fb9c3f6dd58e4bb93e
码魂 <pkptzx@126.com>
2023-01-19 02:33:48 +0800

    支持发送默认的表情图片

--- src/assets/js/biliApi.js
+++ src/assets/js/biliApi.js
@@ -38,11 +38,169 @@ function toCookiesPlainText(cookies_json) {
   }
   return cookies_raw_string;
 }
+const dynamic_emojis=[
+  {
+      "emoji": "赞",
+      "emoticon_unique": "official_147"
+  },
+  {
+      "emoji": "妙啊",
+      "emoticon_unique": "official_109"
+  },
+  {
+      "emoji": "有点东西",
+      "emoticon_unique": "official_113"
+  },
+  {
+      "emoji": "离谱",
+      "emoticon_unique": "official_120"
+  },
+  {
+      "emoji": "很有精神",
+      "emoticon_unique": "official_150"
+  },
+  {
+      "emoji": "泪目",
+      "emoticon_unique": "official_103"
+  },
+  {
+      "emoji": "赢麻了",
+      "emoticon_unique": "official_128"
+  },
+  {
+      "emoji": "钝角",
+      "emoticon_unique": "official_133"
+  },
+  {
+      "emoji": "干杯",
+      "emoticon_unique": "official_149"
+  },
+  {
+      "emoji": "2333",
+      "emoticon_unique": "official_124"
+  },
+  {
+      "emoji": "打call",
+      "emoticon_unique": "official_146"
+  },
+  {
+      "emoji": "多谢款待",
+      "emoticon_unique": "official_148"
+  },
+  {
+      "emoji": "awsl",
+      "emoticon_unique": "official_102"
+  },
+  {
+      "emoji": "笑死",
+      "emoticon_unique": "official_121"
+  },
+  {
+      "emoji": "鸡汤来咯",
+      "emoticon_unique": "official_137"
+  },
+  {
+      "emoji": "雀食",
+      "emoticon_unique": "official_118"
+  },
+  {
+      "emoji": "烦死了",
+      "emoticon_unique": "official_129"
+  },
+  {
+      "emoji": "禁止套娃",
+      "emoticon_unique": "official_108"
+  },
+  {
+      "emoji": "暗中观察",
+      "emoticon_unique": "official_104"
+  },
+  {
+      "emoji": "保熟吗",
+      "emoticon_unique": "official_105"
+  },
+  {
+      "emoji": "比心",
+      "emoticon_unique": "official_106"
+  },
+  {
+      "emoji": "what",
+      "emoticon_unique": "official_114"
+  },
+  {
+      "emoji": "好耶",
+      "emoticon_unique": "official_107"
+  },
+  {
+      "emoji": "咸鱼翻身",
+      "emoticon_unique": "official_110"
+  },
+  {
+      "emoji": "mua",
+      "emoticon_unique": "official_111"
+  },
+  {
+      "emoji": "打扰了",
+      "emoticon_unique": "official_136"
+  },
+  {
+      "emoji": "来了来了",
+      "emoticon_unique": "official_115"
+  },
+  {
+      "emoji": "贴贴",
+      "emoticon_unique": "official_116"
+  },
+  {
+      "emoji": "牛牛牛",
+      "emoticon_unique": "official_117"
+  },
+  {
+      "emoji": "颠个勺",
+      "emoticon_unique": "official_119"
+  },
+  {
+      "emoji": "好家伙",
+      "emoticon_unique": "official_122"
+  },
+  {
+      "emoji": "那我走",
+      "emoticon_unique": "official_123"
+  },
+  {
+      "emoji": "下次一定",
+      "emoticon_unique": "official_125"
+  },
+  {
+      "emoji": "不上Ban",
+      "emoticon_unique": "official_126"
+  },
+  {
+      "emoji": "就这",
+      "emoticon_unique": "official_127"
+  },
+  {
+      "emoji": "上热榜",
+      "emoticon_unique": "official_134"
+  },
+  {
+      "emoji": "中奖喷雾",
+      "emoticon_unique": "official_135"
+  },
+  {
+      "emoji": "我不理解",
+      "emoticon_unique": "official_138"
+  }
+]
 //发送弹幕 返回f屏蔽词 k直播间屏蔽词
 async function send_danmu(roomid, msg) {
-  if (msg.trim().length == 0) {
+  msg = msg.trim();
+  if (msg.length == 0) {
     return;
   }
+  const dynamic_emoji = dynamic_emojis.find(emj=>emj.emoji==msg);
+  const dm_type = dynamic_emoji ? '1':'0';
+  msg = dynamic_emoji ? dynamic_emoji.emoticon_unique : msg;
   const cookies = await getCookies();
   const bili_jct = cookies.bili_jct;
   let cookies_raw_string = toCookiesPlainText(cookies)
@@ -50,6 +208,7 @@ async function send_danmu(roomid, msg) {
     'bubble': '0',
     'msg': msg,
     'color': '16777215',
+    'dm_type': dm_type, //是否为通用图片表情,233,点赞,颠个勺等等的这种
     'mode': '1',
     'fontsize': '25',
     'rnd': String(Math.round(Date.now() / 1000)),
@@ -69,6 +228,7 @@ async function send_danmu(roomid, msg) {
     }
   })
 }
+
 //发送私信
 async function send_msg(receiver_id, msg) {
   const cookies = await getCookies();
@@ -164,6 +324,10 @@ async function getLivesInfo(uids) {
 
 // 获取主播核心数据 range_type=今日1 7日2 近30日3
 // https://api.live.bilibili.com/xlive/app-blink/v1/date/CoreData?platform=web&mobi_app=web&build=1&range_type=1
+
+// 直播间可用表情包
+// https://api.live.bilibili.com/xlive/web-ucenter/v2/emoticon/GetEmoticons?platform=pc&room_id=8094023
+
 async function getCoreData(range_type) {
   const cookies = await getCookies();
   const cookies_raw_string = toCookiesPlainText(cookies);
